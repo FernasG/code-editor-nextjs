9a8f394d39280f7ceeb452c216e2c905dac8ea0e
FernasG <fernandogoul7@gmail.com>
2023-05-13 11:20:13 -0300

    build: add material components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from 'next/head';
-import { Banner } from '@components';
-import styles from '@/styles/Home.module.css';
 import { Inter } from 'next/font/google';
-import BannerImage from '../assets/banner.jpg';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,9 +9,6 @@ export default function Home() {
       <Head>
         <title>Code Editor</title>
       </Head>
-      <main className={styles.container}>
-        <Banner heigth={'100vh'} width={'100%'} image={BannerImage}></Banner>
-      </main>
     </>
   );
 }
