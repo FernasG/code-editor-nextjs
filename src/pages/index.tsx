0c704cf490ff67886ae3bbd1e503424ab0b9de3d
FernasG <fernandogoul7@gmail.com>
2023-05-15 16:50:19 -0300

    build: remove mui packages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
-import { SignBox } from '../components';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,9 +9,6 @@ export default function Home() {
       <Head>
         <title>Code Editor</title>
       </Head>
-      <main className={'container'}>
-        <SignBox></SignBox>        
-      </main>
     </>
   );
 }
